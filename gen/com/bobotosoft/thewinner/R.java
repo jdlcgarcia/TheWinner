5d43497c6c18047d68469cf26b46d886fc8e73b8
Juan de la Cruz García García <jcruzgar@gmail.com>
2014-01-14 21:06:59 +0100

    Arreglado random

--- gen/com/bobotosoft/thewinner/R.java
+++ gen/com/bobotosoft/thewinner/R.java
@@ -21,10 +21,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ico512=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int BaddItem=0x7f080005;
+        public static final int Brandomize=0x7f080004;
+        public static final int Breset=0x7f080003;
+        public static final int ETnewItem=0x7f080000;
+        public static final int SVlistContainer=0x7f080001;
+        public static final int TVmainList=0x7f080002;
+        public static final int TVwinner=0x7f080006;
+        public static final int action_settings=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/bobotosoft/thewinner/MainActivity.java
+++ src/com/bobotosoft/thewinner/MainActivity.java
@@ -1,15 +1,50 @@
 package com.bobotosoft.thewinner;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity{
+	TextView mainList,winner;
+	EditText newItem;
+	List<String> actualList;
+	Button BaddItem;
+	TextView.OnEditorActionListener enterListener = new TextView.OnEditorActionListener(){
+		public boolean onEditorAction(TextView exampleView, int actionId, KeyEvent event) {
+			   if (actionId == EditorInfo.IME_NULL  
+			      && event.getAction() == KeyEvent.ACTION_DOWN) { 
+				   addItem(BaddItem);//match this behavior to your 'Send' (or Confirm) button
+			   }
+			   return true;
+			}
+	};
+	
+	  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		BaddItem = (Button)findViewById(R.id.BaddItem);
+		mainList = (TextView)findViewById(R.id.TVmainList);
+		newItem = (EditText)findViewById(R.id.ETnewItem);
+		winner = (TextView)findViewById(R.id.TVwinner);
+		actualList = new ArrayList<String>();
+		newItem.setOnEditorActionListener(enterListener);
+		
+		
 	}
 
 	@Override
@@ -19,4 +54,37 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	
+	public void addItem(View view) {
+		String nuevoItem = newItem.getText().toString();
+		if(nuevoItem != "")
+		{
+			actualList.add(newItem.getText().toString());
+			String itemsToAdd="";
+		    Collections.sort(actualList);
+		    Iterator<String> iterator = actualList.iterator();
+			while (iterator.hasNext()) {
+				itemsToAdd+=iterator.next().toString()+"\n";
+			}
+			mainList.setText(itemsToAdd);
+		    newItem.setText("");
+		}
+	}
+	
+	public void resetList(View view) {
+		actualList.clear();
+		mainList.setText("");
+	    newItem.setText("");
+	    winner.setText("");
+	}
+	
+	public void getTheWinner(View view) {
+		winner.setText(actualList.get(aleatorio(0,actualList.size()-1)));
+	    
+	 }
+	
+	public static int aleatorio(int max,int min){
+		return min + (int)(Math.random() * ((max- min) + 1));
+}
 }
